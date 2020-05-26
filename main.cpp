b506e8929f13ccb34ac11d3b66258035a78acb3b
CheremnykhYA <yurchest@gmail.com>
2020-05-26 12:53:38 +0300

    2

--- main.cpp
+++ main.cpp
@@ -7,13 +7,20 @@ using namespace std;
 
 int main()
 {
+    uint8_t u8 = 0x42;
+    uint16_t u16 = 0x42;
     uint32_t u32 = 0x42;
-cout << "u32 bytes: ";
-print_in_hex(&u32, sizeof(u32));
-cout << '\n';
 
+    cout << "u8 bytes: ";
+    print_in_hex( &u8, sizeof(u8) );
+    cout << '\n';
 
+    cout << "u16 bytes: ";
+    print_in_hex( &u16, sizeof(u16) );
+    cout << '\n';
 
-
+    cout << "u32 bytes: ";
+    print_in_hex( &u32, sizeof(u32) );
+    cout << '\n';
 
 }
